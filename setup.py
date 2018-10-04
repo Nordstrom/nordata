3502b12f7816910436a1828f35c1795efa25a468
Buker, Nick <Nick.Buker@nordstrom.com>
2018-10-04 10:33:24 -0700

    update setup add manifest

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
+from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
-def setup():
-    return
+setup(
+    name='nuttypy',
+    version='0.0.1',
+    install_requires=['boto3', 'psycopg2-binary'], # TODO
+    description='Simple functions for accessing AWS Redshift and S3',
+    py_modules=['nuttypy'],
+    package_dir={'': 'NuttyPy'},
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/nickbuker/nuttypy',
+    author='Nick Buker',
+    author_email='nickbuker@gmail.com',
+)

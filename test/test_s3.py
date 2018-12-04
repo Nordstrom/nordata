e7a99a5567454e061d36ad5657596385850cedc1
Buker, Nick <Nick.Buker@nordstrom.com>
2018-12-04 08:38:17 -0800

    add s3 test comments

--- test/test_s3.py
+++ test/test_s3.py
@@ -3,8 +3,8 @@ import boto3
 from ..nordata import _s3 as s3
 
 
-def test_s3_create_session_type():
-    
+def test_create_session_type():
+    # test whether _create_session() returns the proper type
     assert isinstance(s3.create_session(), boto3.session.Session)
 
 
@@ -22,12 +22,14 @@ download_upload_TypeError_args = [
 
 @pytest.mark.parametrize('s3_filepath,local_filepath', download_upload_TypeError_args)
 def test_s3_download_type_error(s3_filepath, local_filepath):
+    # test whether s3_download() raises the proper error
     with pytest.raises(TypeError):
         s3.s3_download(bucket='test', s3_filepath=s3_filepath, local_filepath=local_filepath)
 
 
 @pytest.mark.parametrize('s3_filepath,local_filepath', download_upload_TypeError_args)
 def test_s3_upload_type_error(s3_filepath, local_filepath):
+    # test whether s3_upload() raises the proper error
     with pytest.raises(TypeError):
         s3.s3_upload(bucket='test', local_filepath=local_filepath, s3_filepath=s3_filepath)
 
@@ -42,12 +44,14 @@ download_upload_ValueError_args = [
 
 @pytest.mark.parametrize('s3_filepath,local_filepath', download_upload_ValueError_args)
 def test_s3_download_value_error(s3_filepath, local_filepath):
+    # test whether s3_download() raises the proper error
     with pytest.raises(ValueError):
         s3.s3_download(bucket='test', s3_filepath=s3_filepath, local_filepath=local_filepath)
 
 
 @pytest.mark.parametrize('s3_filepath,local_filepath', download_upload_ValueError_args)
 def test_s3_upload_value_error(s3_filepath, local_filepath):
+    # test whether s3_upload() raises the proper error
     with pytest.raises(ValueError):
         s3.s3_upload(bucket='test', local_filepath=local_filepath, s3_filepath=s3_filepath)
 
@@ -57,10 +61,12 @@ delete_TypeError_args = [(1,), ([1],)]
 
 @pytest.mark.parametrize('s3_filepath', delete_TypeError_args)
 def test_s3_delete_type_error(s3_filepath):
+    # test whether s3_delete() raises the proper error
     with pytest.raises(TypeError):
         s3.s3_delete(bucket='test', s3_filepath=s3_filepath)
 
 
 def test_s3_delete_value_error():
+    # test whether s3_delete() raises the proper error
     with pytest.raises(ValueError):
         s3.s3_delete(bucket='test', s3_filepath=['*'])
